049746c92b08449929981cde05a74b85d23a0d28
Marcelo <marcelovqvd@gmail.com>
2024-01-10 10:18:51 -0300

    main styles

--- src/app/components/Widget.tsx
+++ src/app/components/Widget.tsx
@@ -47,6 +47,42 @@ export default function Widget() {
           </div>
         </div>
       </div>
+
+      <div>
+        <div className='bg-zinc-950 px-5 py-2 text-sm text-zinc-400'>
+          Antigas
+        </div>
+
+        <div className='divide-y-2 divide-zinc-950'>
+          <div className='bg-zinc-900 px-8 py-4 flex items-start gap-6'>
+            <Rocket className='w-6 h-6 text-violet-500 mt-3' />
+            <div className='flex-1 flex flex-col gap-2'>
+              <p className='text-sm leading-relaxed text-zinc-400'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Delectus, voluptates.
+              </p>
+              <div className='text-xxs tetx-zinc-400 flez items-center gap-1'>
+                <span>Convite</span>
+                <span>Há 3 minutos</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='bg-zinc-900 px-8 py-4 flex items-start gap-6'>
+            <Rocket className='w-6 h-6 text-violet-500 mt-3' />
+            <div className='flex-1 flex flex-col gap-2'>
+              <p className='text-sm leading-relaxed text-zinc-400'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Delectus, voluptates.
+              </p>
+              <div className='text-xxs tetx-zinc-400 flez items-center gap-1'>
+                <span>Convite</span>
+                <span>Há 3 minutos</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
