3cdc868acd76ce02b3ca305ef534bf6a028e195e
Marcelo <marcelovqvd@gmail.com>
2024-01-10 10:16:06 -0300

    margin

--- src/app/components/Widget.tsx
+++ src/app/components/Widget.tsx
@@ -17,16 +17,32 @@ export default function Widget() {
           Recentes
         </div>
 
-        <div className='bg-zinc-900 px-8 py-4 flex items-start gap-6'>
-          <Rocket className='w-6 h-6 text-violet-500 mt-3' />
-          <div className='flex-1 flex flex-col gap-2'>
-            <p className='text-sm leading-relaxed text-zinc-100'>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Delectus, voluptates.
-            </p>
-            <div className='text-xxs tetx-zinc-400 flez items-center gap-1'>
-              <span>Convite</span>
-              <span>Há 3 minutos</span>
+        <div className='divide-y-2 divide-zinc-950'>
+          <div className='bg-zinc-900 px-8 py-4 flex items-start gap-6'>
+            <Rocket className='w-6 h-6 text-violet-500 mt-3' />
+            <div className='flex-1 flex flex-col gap-2'>
+              <p className='text-sm leading-relaxed text-zinc-100'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Delectus, voluptates.
+              </p>
+              <div className='text-xxs tetx-zinc-400 flez items-center gap-1'>
+                <span>Convite</span>
+                <span>Há 3 minutos</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='bg-zinc-900 px-8 py-4 flex items-start gap-6'>
+            <Rocket className='w-6 h-6 text-violet-500 mt-3' />
+            <div className='flex-1 flex flex-col gap-2'>
+              <p className='text-sm leading-relaxed text-zinc-100'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Delectus, voluptates.
+              </p>
+              <div className='text-xxs tetx-zinc-400 flez items-center gap-1'>
+                <span>Convite</span>
+                <span>Há 3 minutos</span>
+              </div>
             </div>
           </div>
         </div>
