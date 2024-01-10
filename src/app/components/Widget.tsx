b1494a0d4a30dc134aa86fcb5264c0181cfff8db
Marcelo <marcelovqvd@gmail.com>
2024-01-10 10:27:03 -0300

    main layout

--- src/app/components/Widget.tsx
+++ src/app/components/Widget.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Rocket } from "lucide-react"
+import { Rocket, X, Check } from "lucide-react"
 
 export default function Widget() {
   return (
@@ -43,6 +43,14 @@ export default function Widget() {
                 <span>Convite</span>
                 <span>Há 3 minutos</span>
               </div>
+            </div>
+            <div className='flex gap-2 self-center'>
+              <button className='w-8 h-8 rounded flex items-center justify-center bg-zinc-800 hover:bg-zinc-700'>
+                <X className='w-3 h-3 text-zinc-50' />
+              </button>
+              <button className='w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-zinc-600'>
+                <Check className='w-3 h-3 text-zinc-50' />
+              </button>
             </div>
           </div>
         </div>
