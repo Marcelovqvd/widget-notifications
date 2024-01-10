2c27419b3f5d038a752e457690f2d94fa3cc3672
Marcelo <marcelovqvd@gmail.com>
2024-01-10 10:42:14 -0300

    tema light/dark

--- src/app/components/Widget.tsx
+++ src/app/components/Widget.tsx
@@ -5,7 +5,7 @@ import { Rocket, X, Check } from "lucide-react"
 export default function Widget() {
   return (
     <div className='w-[448px] rounded overflow-hidden'>
-      <div className='bg-zinc-800 py-4 px-6'>
+      <div className='bg-zinc-200 dark:bg-zinc-800 py-4 px-6'>
         <span className='font-bold'>Notificações</span>
         <button className='text-violent-500 font-bold text-xs hover:text-violet-400'>
           MARCAR TODAS COMO VISTAS
@@ -13,15 +13,15 @@ export default function Widget() {
       </div>
 
       <div>
-        <div className='bg-zinc-950 px-5 py-2 text-sm text-zinc-400'>
+        <div className='bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-500 dark:text-zinc-400'>
           Recentes
         </div>
 
-        <div className='divide-y-2 divide-zinc-950'>
-          <div className='bg-zinc-900 px-8 py-4 flex items-start gap-6'>
+        <div className='divide-y-2 divide-zinc-300 dark:divide-zinc-950'>
+          <div className='bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6'>
             <Rocket className='w-6 h-6 text-violet-500 mt-3' />
             <div className='flex-1 flex flex-col gap-2'>
-              <p className='text-sm leading-relaxed text-zinc-100'>
+              <p className='text-sm leading-relaxed text-zinc-600 dark:text-zinc-100'>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Delectus, voluptates.
               </p>
@@ -32,10 +32,10 @@ export default function Widget() {
             </div>
           </div>
 
-          <div className='bg-zinc-900 px-8 py-4 flex items-start gap-6'>
+          <div className='bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6'>
             <Rocket className='w-6 h-6 text-violet-500 mt-3' />
             <div className='flex-1 flex flex-col gap-2'>
-              <p className='text-sm leading-relaxed text-zinc-100'>
+              <p className='text-sm leading-relaxed text-zinc-600 dark:text-zinc-100'>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Delectus, voluptates.
               </p>
@@ -45,7 +45,7 @@ export default function Widget() {
               </div>
             </div>
             <div className='flex gap-2 self-center'>
-              <button className='w-8 h-8 rounded flex items-center justify-center bg-zinc-800 hover:bg-zinc-700'>
+              <button className='w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-700'>
                 <X className='w-3 h-3 text-zinc-50' />
               </button>
               <button className='w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-zinc-600'>
@@ -57,15 +57,15 @@ export default function Widget() {
       </div>
 
       <div>
-        <div className='bg-zinc-950 px-5 py-2 text-sm text-zinc-400'>
+        <div className='bg-zinc-300 dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-500 dark:text-zinc-400'>
           Antigas
         </div>
 
-        <div className='divide-y-2 divide-zinc-950'>
-          <div className='bg-zinc-900 px-8 py-4 flex items-start gap-6'>
+        <div className='divide-y-2 divide-zinc-300 dark:divide-zinc-950'>
+          <div className='bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6'>
             <Rocket className='w-6 h-6 text-violet-500 mt-3' />
             <div className='flex-1 flex flex-col gap-2'>
-              <p className='text-sm leading-relaxed text-zinc-400'>
+              <p className='text-sm leading-relaxed text-zinc-600 dark:text-zinc-100'>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Delectus, voluptates.
               </p>
@@ -76,10 +76,10 @@ export default function Widget() {
             </div>
           </div>
 
-          <div className='bg-zinc-900 px-8 py-4 flex items-start gap-6'>
+          <div className='bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6'>
             <Rocket className='w-6 h-6 text-violet-500 mt-3' />
             <div className='flex-1 flex flex-col gap-2'>
-              <p className='text-sm leading-relaxed text-zinc-400'>
+              <p className='text-sm leading-relaxed text-zinc-600 dark:text-zinc-100'>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Delectus, voluptates.
               </p>
@@ -87,6 +87,14 @@ export default function Widget() {
                 <span>Convite</span>
                 <span>Há 3 minutos</span>
               </div>
+            </div>
+            <div className='flex gap-2 self-center'>
+              <button className='w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-700'>
+                <X className='w-3 h-3 text-zinc-50' />
+              </button>
+              <button className='w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-zinc-600'>
+                <Check className='w-3 h-3 text-zinc-50' />
+              </button>
             </div>
           </div>
         </div>
